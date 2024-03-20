54fd76dcf665c5d305184d94f7e5441e509c1158
Joe Brew <joebrew@gmail.com>
2024-03-20 14:13:31 +0100

    overhaul

--- build.R
+++ build.R
@@ -1,4 +1,4 @@
 bookdown::render_book('index.Rmd', 'bookdown::gitbook')
-bookdown::render_book('index.Rmd', 'bookdown::pdf_book')
-bookdown::render_book('index.Rmd', 'bookdown::epub_book')
+# bookdown::render_book('index.Rmd', 'bookdown::pdf_book')
+# bookdown::render_book('index.Rmd', 'bookdown::epub_book')
 
